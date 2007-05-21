4c9b73ec145bbcab0eedaf8ab850a63578da7608
jdaussel <jdaussel@afec0d47-af30-0410-a19f-fe2ccf33c325>
2007-05-21 11:17:18 +0000

    Release 1.6.1

--- trunk/pyscard/src/setup.py
+++ trunk/pyscard/src/setup.py
@@ -59,7 +59,7 @@ else:
 
 
 kw = {'name':"pyscard",
-      'version':"1.6.0",
+      'version':"1.6.1",
       'description':"Smartcard module for Python.",
       'author':"Jean-Daniel Aussel",
       'author_email':"aussel.jean-daniel@gemalto.com",
@@ -104,7 +104,7 @@ kw = {'name':"pyscard",
 if hasattr(core, 'setup_keywords'):
     if 'classifiers' in core.setup_keywords:
         kw['classifiers'] = [
-          'Development Status :: 1.6.0 - Release',
+          'Development Status :: 1.6.1 - Release',
           'License :: GNU LESSER GENERAL PUBLIC LICENSE',
           'Intended Audience :: Developers',
           'Operating System :: Unix',
