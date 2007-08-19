b0d67bf5fd6720f6df6ae56e4eedfe3f3bb18998
jdaussel <jean-daniel.aussel@gemalto.com>
2007-08-19 07:23:30 +0000

    Updated copyright notice

--- smartcard/Examples/framework/sample_apduTracerInterpreter.py
+++ smartcard/Examples/framework/sample_apduTracerInterpreter.py
@@ -2,23 +2,25 @@
 """
 Sample script that defines a custom card connection observer.
 
+__author__ = "http://www.gemalto.com"
+
 Copyright 2001-2007 gemalto
 Author: Jean-Daniel Aussel, mailto:jean-daniel.aussel@gemalto.com
 
-This file is part of scard-python.
+This file is part of pyscard.
 
-scard-python is free software; you can redistribute it and/or modify
+pyscard is free software; you can redistribute it and/or modify
 it under the terms of the GNU Lesser General Public License as published by
 the Free Software Foundation; either version 2.1 of the License, or
 (at your option) any later version.
 
-scard-python is distributed in the hope that it will be useful,
+pyscard is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU Lesser General Public License for more details.
 
 You should have received a copy of the GNU Lesser General Public License
-along with scard-python; if not, write to the Free Software
+along with pyscard; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 """
 from smartcard.CardType import AnyCardType
