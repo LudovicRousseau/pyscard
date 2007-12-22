6f4ae27be82a81a12dae56c5e0262128026e17de
jdaussel <jean-daniel.aussel@gemalto.com>
2007-12-22 15:03:09 +0000

    Added swig_opts support to python 2.3, e.g. for Max OS X darwin

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ elif get_platform() in ('linux-i586', 'linux-i686', 'linux-x86_64'):
     platform_include_dirs=['/usr/include/PCSC']
     platform_extra_compile_args=[]#['-ggdb', '-O0']
     platform_extra_link_args=[]#['-ggdb']
-elif get_platform() in ('macosx-10.3-fat', 'darwin-8.9.1-i386', 'macosx-10.5-i386'):
+elif get_platform() in ('macosx-10.3-fat', 'darwin-8.9.1-i386', 'darwin-8.10.1-i386', 'macosx-10.5-i386'):
     platform__cc_defines=[('PCSCLITE', '1'),('__APPLE__','1')]
     platform_swig_opts=['-DPCSCLITE', '-D__APPLE__']
     platform_sources=[]
@@ -60,6 +60,64 @@ elif get_platform() in ('macosx-10.3-fat', 'darwin-8.9.1-i386', 'macosx-10.5-i38
 else:
     sys.exit("unsupported platform: " + get_platform() )
 
+
+class _pyscardBuildExt(build_ext):
+    '''Specialization of build_ext to enable swig_opts'''
+if sys.version_info < (2,4):
+
+    # This copy of swig_sources is from Python 2.3.
+    # This is to add support of swig_opts for Python 2.3
+    # (in particular for MacOS X darwin that comes with Python 2.4)
+
+    def swig_sources (self, sources):
+
+        """Walk the list of source files in 'sources', looking for SWIG
+        interface (.i) files.  Run SWIG on all that are found, and
+        return a modified 'sources' list with SWIG source files replaced
+        by the generated C (or C++) files.
+        """
+
+        new_sources = []
+        swig_sources = []
+        swig_targets = {}
+
+        # XXX this drops generated C/C++ files into the source tree, which
+        # is fine for developers who want to distribute the generated
+        # source -- but there should be an option to put SWIG output in
+        # the temp dir.
+
+        if self.swig_cpp:
+            target_ext = '.cpp'
+        else:
+            target_ext = '.c'
+
+        for source in sources:
+            (base, ext) = os.path.splitext(source)
+            if ext == ".i":             # SWIG interface file
+                new_sources.append(base + target_ext)
+                swig_sources.append(source)
+                swig_targets[source] = new_sources[-1]
+            else:
+                new_sources.append(source)
+
+        if not swig_sources:
+            return new_sources
+
+        swig = self.find_swig()
+        swig_cmd = [swig, "-python"]
+        if self.swig_cpp:
+            swig_cmd.append("-c++")
+
+        swig_cmd += platform_swig_opts 
+
+        for source in swig_sources:
+            target = swig_targets[source]
+            self.announce("swigging %s to %s" % (source, target))
+            self.spawn(swig_cmd + ["-o", target, source])
+
+        return new_sources
+    
+    build_ext.swig_sources = swig_sources
 
 kw = {'name':"pyscard",
       'version':"1.6.4",
@@ -101,6 +159,7 @@ kw = {'name':"pyscard",
                              extra_compile_args=platform_extra_compile_args,
                              extra_link_args=platform_extra_link_args,
                              swig_opts=['-outdir','smartcard/scard']+platform_swig_opts)],
+      'cmdclass':{'build_ext': _pyscardBuildExt}
      }
 
 # If we're running >Python 2.3, add extra information
@@ -118,5 +177,11 @@ if hasattr(core, 'setup_keywords'):
         kw['download_url'] = ('http://sourceforge.net/projects/pyscard/'
                               '%s-%s.zip' % (kw['name'], kw['version']) )
 
+
 core.setup(**kw)
+
+
+
+
+
 
