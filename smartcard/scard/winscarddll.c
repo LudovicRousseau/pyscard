7866ba83e78c7a030939c28081bcd655ace8cf7e
jdaussel <jean-daniel.aussel@gemalto.com>
2007-12-28 09:58:45 +0000

    Only include stdint.h on Mac OS X

--- smartcard/scard/winscarddll.c
+++ smartcard/scard/winscarddll.c
@@ -19,7 +19,9 @@ along with pyscard; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 ==============================================================================*/
 #include <stdio.h>
-#include <stdint.h>
+#ifdef __APPLE__
+    #include <stdint.h>
+#endif
 #include "pcsctypes.h"
 #include "winscarddll.h"
 
