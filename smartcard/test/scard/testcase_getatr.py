163e65db37549789bfb97a48baf2d3f54eac4fb1
Ludovic Rousseau <ludovic.rousseau@free.fr>
2009-01-13 09:57:28 +0000

    accept T=0 ot T=1 cards

--- smartcard/test/scard/testcase_getatr.py
+++ smartcard/test/scard/testcase_getatr.py
@@ -59,7 +59,7 @@ class testcase_getATR(unittest.TestCase):
     def _getATR(self, r):
         if r<len(expectedATRs) and []!=expectedATRs[r]:
             hresult, hcard, dwActiveProtocol = SCardConnect(
-                self.hcontext, self.readers[r], SCARD_SHARE_SHARED, SCARD_PROTOCOL_T0 )
+                self.hcontext, self.readers[r], SCARD_SHARE_SHARED, SCARD_PROTOCOL_T0 | SCARD_PROTOCOL_T1)
             self.assertEquals(hresult, 0)
 
             try:
