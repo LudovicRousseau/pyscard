7c24de3e029226f577f10f6127fbfb09cf542f43
Ludovic Rousseau <ludovic.rousseau@free.fr>
2009-01-13 13:21:28 +0000

    The PC/SC header files are not in a PCSC directory but found using -framework PCSC

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ elif 'darwin' in get_platform() or 'macosx-10.3' in get_platform() or 'macosx-10
     platform_swig_opts=[ '-DPCSCLITE', '-D__APPLE__', '-D__TIGER__' ]
     platform_sources=[]
     platform_libraries=[]
-    platform_include_dirs=['PCSC']
+    platform_include_dirs=[]
     platform_extra_compile_args=['-v','-framework', 'PCSC', '-arch', 'i386', '-arch', 'ppc', '-ggdb', '-O0']
     platform_extra_link_args=['-arch', 'i386', '-arch', 'ppc','-ggdb']
 
@@ -63,7 +63,7 @@ elif 'macosx-10.5' in get_platform():
     platform_swig_opts=[ '-DPCSCLITE', '-D__APPLE__', '-D__LEOPARD__' ]
     platform_sources=[]
     platform_libraries=[]
-    platform_include_dirs=['PCSC']
+    platform_include_dirs=[]
     platform_extra_compile_args=['-v','-framework', 'PCSC', '-arch', 'i386', '-arch', 'ppc', '-ggdb', '-O0']
     platform_extra_link_args=['-arch', 'i386', '-arch', 'ppc','-ggdb']
 else:
