ee6c1d49765c035b8bd5ad00fdd3b5dadef117e7
ludov <ludov@afec0d47-af30-0410-a19f-fe2ccf33c325>
2009-02-23 08:47:51 +0000

    Sample script to send 2 APDU

---
+++ trunk/pyscard/src/smartcard/Examples/simple/simpleAPDU.py
@@ -0,0 +1,56 @@
+#! /usr/bin/env python
+"""
+Sample script to send 2 APDU
+
+__author__ = "Ludovic Rousseau"
+
+Copyright 2009
+Author: Ludovic Rousseau, mailto:ludovic.rousseau@free.fr
+
+This file is part of pyscard.
+
+pyscard is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation; either version 2.1 of the License, or
+(at your option) any later version.
+
+pyscard is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with pyscard; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+"""
+
+import sys
+from smartcard.System import readers
+
+# define the APDUs used in this script
+SELECT_APPLET = [0x00, 0xA4, 0x04, 0x00, 0x06, 0xA0, 0x00, 0x00, 0x00,
+        0x18, 0xFF]
+GET_TIME = [0x80, 0x38, 0x00, 0xA0]
+
+try:
+    # get all the available readers
+    r = readers()
+    print "Available readers: ", r
+
+    # by default we use the first reader
+    i = 0
+    if len(sys.argv) > 1:
+        i = int(sys.argv[1])
+    print "Using: %s" % r[i]
+
+    connection = r[i].createConnection()
+    connection.connect()
+
+    data, sw1, sw2 = connection.transmit(SELECT_APPLET)
+    print "Select Applet: %02X %02X" % (sw1, sw2)
+
+    data, sw1, sw2 = connection.transmit(GET_TIME)
+    print "Get Time: %02X %02X" % (sw1, sw2)
+
+except:
+    print sys.exc_info()[1]
