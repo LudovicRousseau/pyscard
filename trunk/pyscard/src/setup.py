a00664dd720bb795ab853d472593bc229cc66537
jdaussel <jdaussel@afec0d47-af30-0410-a19f-fe2ccf33c325>
2009-03-12 16:18:40 +0000

    Updated release from 1.6.7 to 1.6.8 to prepare next delivery

--- trunk/pyscard/src/setup.py
+++ trunk/pyscard/src/setup.py
@@ -135,7 +135,7 @@ if sys.version_info < (2,4):
     build_ext.swig_sources = swig_sources
 
 kw = {'name':"pyscard",
-      'version':"1.6.7",
+      'version':"1.6.8",
       'description':"Smartcard module for Python.",
       'author':"Jean-Daniel Aussel",
       'author_email':"aussel.jean-daniel@gemalto.com",
@@ -181,7 +181,7 @@ kw = {'name':"pyscard",
 if hasattr(core, 'setup_keywords'):
     if 'classifiers' in core.setup_keywords:
         kw['classifiers'] = [
-          'Development Status :: 1.6.7 - Release',
+          'Development Status :: 1.6.8 - Release',
           'License :: GNU LESSER GENERAL PUBLIC LICENSE',
           'Intended Audience :: Developers',
           'Operating System :: Unix',
