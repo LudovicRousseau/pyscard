b7046c5a264d959ee80de8e18266aa0902880862
ludov <ludov@afec0d47-af30-0410-a19f-fe2ccf33c325>
2009-11-22 15:24:17 +0000

    pep8: E401 multiple imports on one line

--- trunk/pyscard/src/smartcard/pcsc/PCSCCardRequest.py
+++ trunk/pyscard/src/smartcard/pcsc/PCSCCardRequest.py
@@ -22,7 +22,8 @@ along with pyscard; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 """
 
-import threading, time
+import threading
+import time
 
 from smartcard.AbstractCardRequest import AbstractCardRequest
 from smartcard.Exceptions import CardRequestTimeoutException, CardRequestException, ListReadersException
