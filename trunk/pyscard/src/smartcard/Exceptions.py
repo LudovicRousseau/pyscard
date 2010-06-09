12b2df03e4e870299e994acb9da05385a12cda9e
ludov <ludov@afec0d47-af30-0410-a19f-fe2ccf33c325>
2010-06-09 11:46:00 +0000

    make pep8 happy

--- trunk/pyscard/src/smartcard/Exceptions.py
+++ trunk/pyscard/src/smartcard/Exceptions.py
@@ -24,6 +24,7 @@ along with pyscard; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 """
 
+
 class SmartcardException:
     """Base class for smartcard exceptions.
 
@@ -31,52 +32,73 @@ class SmartcardException:
     shield scard (i.e. PCSC) exceptions raised by the scard module.
 
     """
-    def __init__( self, message="" ):
+
+    def __init__(self, message=""):
         self.message = message
+
     def __str__(self):
-        return repr( 'Smartcard Exception: ' + self.message + '!' )
+        return repr('Smartcard Exception: ' + self.message + '!')
+
 
 class CardConnectionException(SmartcardException):
     """Raised when a CardConnection class method fails."""
-    def __init__( self, message="" ):
-        SmartcardException.__init__( self, message )
+
+    def __init__(self, message=""):
+        SmartcardException.__init__(self, message)
+
 
 class CardRequestException(SmartcardException):
     """Raised when a CardRequest wait fails."""
-    def __init__( self, message="" ):
-        SmartcardException.__init__( self, message )
+
+    def __init__(self, message=""):
+        SmartcardException.__init__(self, message)
+
 
 class CardRequestTimeoutException(SmartcardException):
     """Raised when a CardRequest times out."""
-    def __init__( self, message="" ):
-        SmartcardException.__init__( self, "Time-out during card request" )
+
+    def __init__(self, message=""):
+        SmartcardException.__init__(self, "Time-out during card request")
+
 
 class CardServiceException(SmartcardException):
     """Raised when a CardService class method fails."""
-    def __init__( self, message="" ):
-        SmartcardException.__init__( self, message )
+
+    def __init__(self, message=""):
+        SmartcardException.__init__(self, message)
+
 
 class InvalidATRMaskLengthException(SmartcardException):
     """Raised when an ATR mask does not match an ATR length."""
-    def __init__( self, mask="" ):
-        SmartcardException.__init__( self, 'Invalid ATR mask length: ' + mask )
+
+    def __init__(self, mask=""):
+        SmartcardException.__init__(self, 'Invalid ATR mask length: ' + mask)
+
 
 class InvalidReaderException(SmartcardException):
     """Raised when trying to acces an invalid smartcard reader."""
-    def __init__( self, readername="" ):
-        SmartcardException.__init__( self, 'Invalid reader: ' + readername )
+
+    def __init__(self, readername=""):
+        SmartcardException.__init__(self, 'Invalid reader: ' + readername)
+
 
 class ListReadersException(SmartcardException):
     """Raised when smartcard readers cannot be listed."""
-    def __init__( self, message="" ):
-        SmartcardException.__init__( self, 'failed to list readers' )
+
+    def __init__(self, message=""):
+        SmartcardException.__init__(self, 'failed to list readers')
+
 
 class NoCardException(SmartcardException):
     """Raised when no card in is present in reader."""
-    def __init__( self, message="" ):
-        SmartcardException.__init__( self, 'Unable to connect to card or no card in reader' )
+
+    def __init__(self, message=""):
+        SmartcardException.__init__(self,
+            'Unable to connect to card or no card in reader')
+
 
 class NoReadersException(SmartcardException):
     """Raised when the system has no smartcard reader."""
-    def __init__( self, message="" ):
-        SmartcardException.__init__( self, 'no readers found' )
+
+    def __init__(self, message=""):
+        SmartcardException.__init__(self, 'no readers found')
