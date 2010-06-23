21e2a4b092013621e79cbad92be71df7179407fd
jdaussel <jdaussel@afec0d47-af30-0410-a19f-fe2ccf33c325>
2010-06-23 11:52:22 +0000

    Updated version number to release 1.6.12

--- trunk/pyscard/src/setup.py
+++ trunk/pyscard/src/setup.py
@@ -152,7 +152,7 @@ if sys.version_info < (2, 4):
     build_ext.swig_sources = swig_sources
 
 kw = {'name': "pyscard",
-      'version': "1.6.10",
+      'version': "1.6.12",
       'description': "Smartcard module for Python.",
       'author': "Jean-Daniel Aussel",
       'author_email': "aussel.jean-daniel@gemalto.com",
@@ -198,7 +198,7 @@ kw = {'name': "pyscard",
 if hasattr(core, 'setup_keywords'):
     if 'classifiers' in core.setup_keywords:
         kw['classifiers'] = [
-          'Development Status :: 1.6.10 - Release',
+          'Development Status :: 1.6.12 - Release',
           'License :: GNU LESSER GENERAL PUBLIC LICENSE',
           'Intended Audience :: Developers',
           'Operating System :: Unix',
