3ed18ae06d46f8439d9df1d6a796ac16ad397d72
jdaussel <jdaussel@afec0d47-af30-0410-a19f-fe2ccf33c325>
2010-08-17 15:03:12 +0000

    Added support for py2exe

---
+++ trunk/pyscard/src/smartcard/Examples/wx/apdumanager/setup.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python
+"""
+Setup script to build a standalone apdumanager.exe executable on windows
+using py2exe. Run: python.exe setup.py py2exe, to build executable file.
+
+__author__ = "http://www.gemalto.com"
+
+Copyright 2001-2010 gemalto
+Author: Jean-Daniel Aussel, mailto:jean-daniel.aussel@gemalto.com
+
+This file is part of pyscard.
+
+pyscard is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation; either version 2.1 of the License, or
+(at your option) any later version.
+
+pyscard is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with pyscard; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+"""
+
+from distutils.core import setup
+import py2exe
+
+from smartcard.wx import ICO_SMARTCARD, ICO_READER
+
+Mydata_files = [('images', ['images/mysmartcard.ico', ICO_SMARTCARD, ICO_READER ])]
+
+
+setup(  windows=['apdumanager.py'],
+        data_files = Mydata_files,
+        options =
+        {
+            "py2exe":{"dll_excludes":["MSVCP90.dll"]}
+        }
+    )
+
+
--- trunk/pyscard/src/smartcard/Examples/wx/cardmonitor/cardmonitor.py
+++ trunk/pyscard/src/smartcard/Examples/wx/cardmonitor/cardmonitor.py
@@ -25,10 +25,29 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 """
 
 import os.path
+import sys
 from smartcard.wx.SimpleSCardApp import *
 from smartcard.wx.SimpleSCardAppEventObserver import SimpleSCardAppEventObserver
 
 ID_TEXT = 10000
+
+def we_are_frozen():
+    """Returns whether we are frozen via py2exe.
+    This will affect how we find out where we are located.
+    From WhereAmI page on py2exe wiki."""
+
+    return hasattr(sys, "frozen")
+
+
+def module_path():
+    """ This will get us the program's directory,
+    even if we are frozen using py2exe. From WhereAmI page on py2exe wiki."""
+
+
+    if we_are_frozen():
+        return os.path.dirname( unicode(sys.executable, sys.getfilesystemencoding( )) )
+
+    return os.path.dirname(unicode(__file__, sys.getfilesystemencoding( )))
 
 
 class SamplePanel(wx.Panel, SimpleSCardAppEventObserver):
@@ -92,7 +111,7 @@ def main(argv):
         appname='A simple card monitoring tool',
         apppanel=SamplePanel,
         appstyle=TR_SMARTCARD | TR_READER,
-        appicon=os.path.join(os.path.dirname(__file__), 'images', 'mysmartcard.ico'),
+        appicon=os.path.join(module_path(), 'images', 'mysmartcard.ico'),
         size=(800, 600))
     app.MainLoop()
 
