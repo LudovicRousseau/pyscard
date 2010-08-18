cfde5542b2131b38cc1291f4c5535b04d0175d29
jdaussel <jean-daniel.aussel@gemalto.com>
2010-08-18 13:40:51 +0000

    Added partial support of remote pcsc readers with pyro

---
+++ smartcard/pyro/server/PyroNameServer.py
@@ -0,0 +1,118 @@
+"""Utility class to start/stop Pyro Name Server.
+
+__author__ = "http://www.gemalto.com"
+
+Copyright 2001-2010 gemalto
+Author: Jean-Daniel Aussel, mailto:jean-daniel.aussel@gemalto.com
+
+This file is part of pyscard.
+
+pyscard is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation; either version 2.1 of the License, or
+(at your option) any later version.
+
+pyscard is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with pyscard; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+"""
+
+import signal
+import sys
+from threading import Thread
+import time
+
+import Pyro.naming
+import Pyro.nsc
+import Pyro.util
+
+class PyroNameServer(Thread):
+    """Thread running the Pyro Name server.
+    """
+
+    def __init__(self,args=[]):
+        """Initialize pyro name server with command line arguments.
+        For a complete list of command line arguments, see pyro documentation
+        for pyro-ns start script."""
+        Thread.__init__( self )
+        self.setDaemon( True )
+        self.setName( 'smartcard.pyro.server.PyroNameServer' )
+        self.args = args
+        self.handler = signal.signal( signal.SIGINT, self )
+
+        _args=[]
+        for arg in self.args: _args.append(arg)
+        Args = Pyro.util.ArgParser()
+        Args.parse(_args,'hkmrvxn:p:b:c:d:s:i:1:2:')
+        self.host = Args.getOpt('n',None)
+        self.bcport = Args.getOpt('b',None)
+        self.bcaddr = Args.getOpt('c',None)
+        self.identification = Args.getOpt('i',None)
+
+    def getShutdownArgs(self):
+        """return command line arguments for shutting down the
+        server; this command line is built from the name server
+        startup arguments."""
+        shutdownArgs = []
+        if self.host: shutdownArgs += [ '-h', self.host ]
+        if self.bcport: shutdownArgs += [ '-p', self.bcport ]
+        if self.bcaddr: shutdownArgs += [ '-c', self.bcaddr ]
+        if self.identification: shutdownArgs += [ '-i', self.identification ]
+
+        return shutdownArgs
+
+    def listall(self):
+        """List pyro namespace."""
+        args = self.getShutdownArgs() + ['listall']
+        Pyro.nsc.main( args )
+
+    def ping(self):
+        """Ping pyro naming server."""
+        args = self.getShutdownArgs() + ['ping']
+        Pyro.nsc.main( args )
+
+    def run(self):
+        """Starts Pyro naming server with command line arguments (see pyro documentation)
+        """
+        args=[]
+        for arg in self.args: args.append(arg)
+        Pyro.naming.main( args )
+
+    def stop(self):
+        """Shutdown pyro naming server."""
+        args = self.getShutdownArgs() + ['shutdown']
+        Pyro.nsc.main( args )
+        self.join()
+
+    def waitStarted(self):
+        """wait until name server is started."""
+        ns = None
+        while not ns:
+            try:
+                time.sleep(3)
+                ns=Pyro.naming.NameServerLocator(identification=self.identification).getNS()
+            except Pyro.errors.NamingError as er:
+                pass
+
+    def __call__(self, signame, sf):
+        """Ctrl+c handler that will gracefully shutdown the server
+        upon Ctrl+C signal"""
+        print 'PyroNameServer Ctrl+C handler'
+        self.stop()
+        time.sleep(1)
+        self.handler( signame, sf )
+
+
+if __name__ == '__main__':
+    import sys
+    pt = PyroNameServer( sys.argv[1:] )
+    pt.start()
+    pt.waitStarted()
+    pt.stop()
+
+
---
+++ smartcard/pyro/server/RemoteReaderServer.py
@@ -0,0 +1,121 @@
+"""RemoteReaderServer: monitor local readers and publish them as pyro 
+Remote Readers.
+
+__author__ = "gemalto http://www.gemalto.com"
+
+Copyright 2001-2010 gemalto
+Author: Jean-Daniel Aussel, mailto:jean-daniel.aussel@gemalto.com
+
+This file is part of pyscard.
+
+pyscard is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation; either version 2.1 of the License, or
+(at your option) any later version.
+
+pyscard is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with pyscard; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+"""
+
+try:
+    import Pyro.core
+    import Pyro.naming
+except:
+    print 'You need pyro (python remote objects) at http://www.xs4all.nl/~irmen/pyro3/'
+    import sys
+    sys.exit()
+
+import signal
+import time
+
+import smartcard.System
+from smartcard.reader.Reader import Reader
+from smartcard.ReaderMonitoring import ReaderMonitor, ReaderObserver
+from smartcard.pyro.server.RemoteCardConnection import RemoteCardConnection
+from smartcard.pyro.server import PyroDaemon
+from smartcard.pyro.server import PyroNameServer
+
+class RemoteReader(Reader,Pyro.core.ObjBase):
+    """Remote reader class that embeds a local reader."""
+
+    def __init__(self,reader):
+        """Encapsulate a local reader and publish as a pyro remote reader.
+
+        reader: the local reader to publish remotely
+        """
+        Pyro.core.ObjBase.__init__(self)
+        self.readerobj = reader
+        self.name = reader.name
+
+    def addtoreadergroup( self, groupname ):
+        """Add reader to a reader group."""
+        self.readerobj.reader.addtoreadergroup( groupname )
+
+    def removefromreadergroup( self, groupname ):
+        """Remove a reader from a reader group"""
+        self.readerobj.reader.removefromreadergroup( groupname )
+
+    def createConnection( self ):
+        """Return a card connection thru the reader."""
+        connection = RemoteCardConnection( self.readerobj.createConnection() )
+        daemon = PyroDaemon.PyroDaemon()
+        uri = daemon.connect(connection)
+        return uri
+
+
+
+class RemoteReaderServer(ReaderObserver):
+    """Monitor local readers, and publish them as remote pyro readers.
+    """
+    def __init__(self):
+        """Starts pyro name server and constructs reader name space."""
+        self.pn = PyroNameServer.PyroNameServer()
+        self.pn.start()
+        self.pn.waitStarted()
+    
+        Pyro.core.initServer()
+        self.ns=Pyro.naming.NameServerLocator().getNS()
+        try:
+            self.ns.createGroup(':pyscard')
+            self.ns.createGroup(':pyscard.smartcard')
+            self.ns.createGroup(':pyscard.smartcard.readers')
+        except Pyro.errors.NamingError as error:
+            print error
+        self.daemon = PyroDaemon.PyroDaemon()
+        self.remotereaders = {}
+
+    def start(self):
+        """Start pyro daemon and accept incoming requests."""
+        self.daemon.start()
+    
+
+    def update(self, observable, (addedreaders, removedreaders)):
+        """Called when a local reader is added or removed.
+        Create remote pyro reader objects for added readers.
+        Delete remote pyro reader objects for removed readers."""
+        for reader in addedreaders:
+            remotereader = RemoteReader(reader)
+            self.remotereaders[reader.name] = remotereader
+            name = "".join(reader.name.split(' '))
+            name = ":pyscard.smartcard.readers."+"".join(name.split('.'))
+            uri = self.daemon.connect(remotereader,name)
+        for reader in removedreaders:
+            remotereader = self.remotereaders[reader.name]
+            self.daemon.disconnect(remotereader)
+            del self.remotereaders[reader.name]
+        self.pn.listall()
+
+
+if __name__ == '__main__':
+    readerserver = RemoteReaderServer()
+    readermonitor = ReaderMonitor()
+    readermonitor.addObserver(readerserver)
+    print 'Reader remote server up and running',
+    print 'Please enter Ctrl+C to stop and exit...'
+    readerserver.start()
