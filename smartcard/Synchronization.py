e184d1e43f62c57b4bad1a078ccb795d2062cd8d
Ludovic Rousseau <ludovic.rousseau@free.fr>
2011-10-19 12:47:30 +0000

    Partly revert revision 571 because of side effect
    The error is:
      File "/Library/Python/2.7/site-packages/smartcard/__init__.py", line 34, in <module>
        from smartcard.System import listReaders
      File "/Library/Python/2.7/site-packages/smartcard/System.py", line 27, in <module>
        import smartcard.reader.ReaderFactory
      File "/Library/Python/2.7/site-packages/smartcard/reader/ReaderFactory.py", line 34, in <module>
        from smartcard.pcsc.PCSCReader import PCSCReader
      File "/Library/Python/2.7/site-packages/smartcard/pcsc/PCSCReader.py", line 25, in <module>
        from smartcard.CardConnectionDecorator import CardConnectionDecorator
      File "/Library/Python/2.7/site-packages/smartcard/CardConnectionDecorator.py", line 27, in <module>
        from smartcard.CardConnection import CardConnection
      File "/Library/Python/2.7/site-packages/smartcard/CardConnection.py", line 28, in <module>
        from smartcard.Observer import Observer
      File "/Library/Python/2.7/site-packages/smartcard/Observer.py", line 81, in <module>
        "countObservers")
      File "/Library/Python/2.7/site-packages/smartcard/Synchronization.py", line 36, in synchronize
        klass.__dict__[name] = synchronized(val)
    TypeError: 'dictproxy' object does not support item assignment

--- smartcard/Synchronization.py
+++ smartcard/Synchronization.py
@@ -36,7 +36,7 @@ def synchronize(klass, names=None):
                 klass.__dict__[name] = synchronized(val)
 
 
-class Synchronization(object):
+class Synchronization:
     # You can create your own self.mutex, or inherit from this class:
 
     def __init__(self):
