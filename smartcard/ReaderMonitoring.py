dcf227cad373a3e7ca73e90e7d4f2e15509b6a37
Ludovic Rousseau <ludovic.rousseau@free.fr>
2015-06-20 15:33:41 +0200

    Pytyon3: support print() function in Python2
    From http://python3porting.com/noconv.html#supporting-the-print-function

--- smartcard/ReaderMonitoring.py
+++ smartcard/ReaderMonitoring.py
@@ -28,6 +28,7 @@ along with pyscard; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 """
 
+from __future__ import print_function
 from threading import Thread, Event, enumerate
 from time import sleep
 import traceback
