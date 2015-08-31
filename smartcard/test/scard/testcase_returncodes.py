5243d096ceb632d745b60eb925c90b47c0158e2e
Ludovic Rousseau <ludovic.rousseau@free.fr>
2015-08-31 21:05:19 +0200

    testcase_returncodes.py: remove extra import
    sys was imported but not used.

--- smartcard/test/scard/testcase_returncodes.py
+++ smartcard/test/scard/testcase_returncodes.py
@@ -29,7 +29,6 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 import unittest
 from smartcard.scard import *
-import sys
 
 
 class testcase_returncodes(unittest.TestCase):
