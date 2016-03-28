9c38ef2f664dc3fb4deea9d19aa0eb16e428be14
Ludovic Rousseau <ludovic.rousseau@free.fr>
2016-03-28 14:10:13 +0200

    test: fix pep8 errors
    test/test_util.py:11:1: E302 expected 2 blank lines, found 1
    test/test_util.py:12:1: W293 blank line contains whitespace
    test/test_util.py:37:11: E128 continuation line under-indented for
    visual indent
    test/test_util.py:41:5: E303 too many blank lines (2)
    test/test_util.py:43:21: E201 whitespace after '['
    test/test_util.py:43:80: E202 whitespace before ']'
    test/test_util.py:43:80: E501 line too long (81 > 79 characters)
    test/test_util.py:47:5: E303 too many blank lines (2)
    test/test_util.py:48:24: E231 missing whitespace after ','
    test/test_util.py:48:29: E231 missing whitespace after ','
    test/test_util.py:48:34: E231 missing whitespace after ','
    test/test_util.py:48:39: E231 missing whitespace after ','
    test/test_util.py:48:44: E231 missing whitespace after ','
    test/test_util.py:48:49: E231 missing whitespace after ','
    test/test_util.py:48:54: E231 missing whitespace after ','
    test/test_util.py:48:59: E231 missing whitespace after ','
    test/test_util.py:48:64: E231 missing whitespace after ','
    test/test_util.py:53:5: E303 too many blank lines (2)
    test/test_util.py:55:21: E201 whitespace after '['
    test/test_util.py:55:62: E202 whitespace before ']'
    test/test_util.py:59:21: E201 whitespace after '['
    test/test_util.py:59:68: E202 whitespace before ']'
    test/test_util.py:63:21: E201 whitespace after '['
    test/test_util.py:63:68: E202 whitespace before ']'
    test/test_util.py:67:21: E201 whitespace after '['
    test/test_util.py:67:44: E202 whitespace before ']'
    test/test_util.py:78:20: E201 whitespace after '['
    test/test_util.py:78:73: E202 whitespace before ']'
    test/test_util.py:83:5: E303 too many blank lines (2)
    test/test_util.py:89:5: E303 too many blank lines (2)

--- test/test_util.py
+++ test/test_util.py
@@ -8,8 +8,9 @@
 import unittest
 from smartcard.util import *
 
+
 class TestUtil(unittest.TestCase):
-    
+
     def test_toBytes(self):
         data_in = "3B 65 00 00 9C 11 01 01 03"
         data_out = [59, 101, 0, 0, 156, 17, 1, 1, 3]
@@ -34,37 +35,34 @@ class TestUtil(unittest.TestCase):
     def test_padd(self):
         data_in = toBytes("3B 65 00 00 9C 11 01 01 03")
         data_out = [0x3B, 0x65, 0, 0, 0x9C, 0x11, 1, 1, 3,
-          0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
+                    0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
         self.assertEqual(padd(data_in, 16), data_out)
-
 
     def test_toASCIIBytes(self):
         data_in = "Number 101"
-        data_out = [ 0x4E, 0x75, 0x6D, 0x62, 0x65, 0x72, 0x20, 0x31, 0x30, 0x31 ]
+        data_out = [0x4E, 0x75, 0x6D, 0x62, 0x65, 0x72, 0x20, 0x31, 0x30, 0x31]
         self.assertEqual(toASCIIBytes(data_in), data_out)
 
-
     def test_toASCIIString(self):
-        data_in = [0x4E,0x75,0x6D,0x62,0x65,0x72,0x20,0x31,0x30,0x31]
+        data_in = [0x4E, 0x75, 0x6D, 0x62, 0x65, 0x72, 0x20, 0x31, 0x30, 0x31]
         data_out = "Number 101"
         self.assertEqual(toASCIIString(data_in), data_out)
 
-
     def test_toGSM3_38Bytes(self):
         data_in = "@Pascal"
-        data_out = [ 0x00, 0x50, 0x61, 0x73, 0x63, 0x61, 0x6C ]
+        data_out = [0x00, 0x50, 0x61, 0x73, 0x63, 0x61, 0x6C]
         self.assertEqual(toGSM3_38Bytes(data_in), data_out)
 
         data_in = u"@ùPascal"
-        data_out = [ 0x00, 0x06, 0x50, 0x61, 0x73, 0x63, 0x61, 0x6C ]
+        data_out = [0x00, 0x06, 0x50, 0x61, 0x73, 0x63, 0x61, 0x6C]
         self.assertEqual(toGSM3_38Bytes(data_in), data_out)
 
         data_in = u"@ùPascal".encode('iso8859-1')
-        data_out = [ 0x00, 0x06, 0x50, 0x61, 0x73, 0x63, 0x61, 0x6C ]
+        data_out = [0x00, 0x06, 0x50, 0x61, 0x73, 0x63, 0x61, 0x6C]
         self.assertEqual(toGSM3_38Bytes(data_in), data_out)
 
         data_in = "1234"
-        data_out = [ 0x31, 0x32, 0x33, 0x34 ]
+        data_out = [0x31, 0x32, 0x33, 0x34]
         self.assertEqual(toGSM3_38Bytes(data_in), data_out)
 
     def test_toHexString(self):
@@ -75,16 +73,14 @@ class TestUtil(unittest.TestCase):
         data_in = 42
         self.assertRaises(TypeError, toHexString, data_in)
 
-        data_in = [ 0x3B, 0x65, 0x00, 0x00, 0x9C, 0x11, 0x01, 0x01, 0x03 ]
+        data_in = [0x3B, 0x65, 0x00, 0x00, 0x9C, 0x11, 0x01, 0x01, 0x03]
         data_out = "3B 65 00 00 9C 11 01 01 03"
         self.assertEqual(toHexString(data_in), data_out)
-
 
     def test_HexListToBinString(self):
         data_in = [1, 2, 3]
         data_out = "\x01\x02\x03"
         self.assertEqual(HexListToBinString(data_in), data_out)
-
 
     def test_BinStringToHexList(self):
         data_in = "\x01\x02\x03"
