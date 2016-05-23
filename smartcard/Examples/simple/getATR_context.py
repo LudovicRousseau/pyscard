1c45eebe5f445c2b7e9bccf7a7849d4020e07edc
Ludovic Rousseau <ludovic.rousseau@free.fr>
2016-05-23 17:02:27 +0200

    getATR_context.py: sample for context use
    Sample code to illustrate of use of a CardConnection context introduced
    in fb276f8a35d0e231699c9f38da2685daa0a4917c using the "with" construction.
    
    See PEP 343 -- The "with" Statement
    https://www.python.org/dev/peps/pep-0343/

---
+++ smartcard/Examples/simple/getATR_context.py
@@ -0,0 +1,49 @@
+#! /usr/bin/env python
+"""
+Sample script that displays the ATR of inserted cards.
+
+__author__ = "http://www.gemalto.com"
+
+Copyright 2001-2012 gemalto
+Author: Jean-Daniel Aussel, mailto:jean-daniel.aussel@gemalto.com
+Copyright 2010 Ludovic Rousseau
+Author: Ludovic Rousseau, mailto:ludovic.rousseau@free.fr
+
+This file is part of pyscard.
+
+pyscard is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation; either version 2.1 of the License, or
+(at your option) any later version.
+
+pyscard is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with pyscard; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+"""
+from __future__ import print_function
+from smartcard.Exceptions import NoCardException
+from smartcard.System import readers
+from smartcard.util import toHexString
+
+# Use a connection context
+# the connection object is automatically destroyed at the end of with block
+
+for reader in readers():
+    with reader.createConnection() as connection:
+        try:
+            connection.connect()
+            print(reader, toHexString(connection.getATR()))
+        except NoCardException:
+            print(reader, 'no card inserted')
+
+#sleep(5)
+
+import sys
+if 'win32' == sys.platform:
+    print('press Enter to continue')
+    sys.stdin.read(1)
