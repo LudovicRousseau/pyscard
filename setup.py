ed2f0c2955c266987507563040d44b55ef162b0a
Ludovic Rousseau <ludovic.rousseau@free.fr>
2018-05-16 17:46:27 +0200

    setup: document support of Python 3.5 and 3.6

--- setup.py
+++ setup.py
@@ -154,6 +154,8 @@ kw = {'name': "pyscard",
           'Programming Language :: Python :: 2.7',
           'Programming Language :: Python :: 3.3',
           'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
           'Topic :: Security',
           ]
       }
