725e57a945f5e6d1129dd39eba9d3e8d9ff08db3
Ludovic Rousseau <ludovic.rousseau@free.fr>
2018-06-11 09:50:07 +0200

    test: make import explicit

--- test/test_SCardGetErrorMessage.py
+++ test/test_SCardGetErrorMessage.py
@@ -6,7 +6,8 @@
 # $ python -m unittest
 
 import unittest
-from smartcard.scard import *
+from smartcard.scard import SCardGetErrorMessage
+from smartcard.scard import SCARD_S_SUCCESS, SCARD_F_INTERNAL_ERROR
 from distutils.util import get_platform
 
 
@@ -15,8 +16,8 @@ class TestError(unittest.TestCase):
     def test_SCardGetErrorMessage(self):
         res = SCardGetErrorMessage(SCARD_S_SUCCESS)
 
-        # do not test on Windows
-        # the error messages are different and localized
+        # do not test on Windows
+        # the error messages are different and localized
         if get_platform() in ('win32', 'win-amd64'):
             return
 
