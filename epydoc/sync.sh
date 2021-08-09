740905978073f3cec1332674e1db9676fce2aff4
Ludovic Rousseau <ludovic.rousseau@free.fr>
2021-08-09 22:06:32 +0200

    Remove useless epydoc/ directory
    The documentation is generated using Sphinx since 2014.

--- epydoc/sync.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-# update the web site at sourceforge.net
-pushd $(dirname $0)/generated
-rsync --recursive --verbose --update epydoc ludov@web.sourceforge.net:/home/project-web/pyscard/htdocs/
-popd
