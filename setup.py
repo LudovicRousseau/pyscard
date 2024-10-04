878c408f59196728fb886afb3edb2ab8c1adea19
Kurt McKee <contactme@kurtmckee.org>
2024-10-04 07:15:14 -0500

    Test Python 3.13
    Python 3.13 will be released in a few days
    (its third and final release candidate was published a few days ago).
    
    This PR adds Python 3.13 to the tox test suite and to CI.

--- setup.py
+++ setup.py
@@ -142,6 +142,7 @@ kw = {
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "Topic :: Security",
     ],
 }
