65b72339d3d8be7eccf283bbce616d9c11bcb9c4
Ludovic Rousseau <ludovic.rousseau@free.fr>
2025-01-12 15:40:56 +0100

    Release 2.2.1
    Signed-off-by: Ludovic Rousseau <ludovic.rousseau@free.fr>

--- src/smartcard/doc/conf.py
+++ src/smartcard/doc/conf.py
@@ -18,9 +18,9 @@ copyright = "2014, Jean-Daniel Aussel, Ludovic Rousseau"
 # built documents.
 #
 # The short X.Y version.
-version = "2.2.0"
+version = "2.2.1"
 # The full version, including alpha/beta/rc tags.
-release = "2.2.0"
+release = "2.2.1"
 
 pygments_style = "sphinx"
 
