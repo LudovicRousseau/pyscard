791094cce7c2b2addd9c14c1cdb0b83117fb9978
Ludovic Rousseau <ludovic.rousseau@free.fr>
2025-07-20 15:08:56 +0200

    Fix pylint warning C0123
    src/smartcard/sw/ISO7816_9ErrorChecker.py:95:15: C0123: Use isinstance() rather than type() for a typecheck. (unidiomatic-typecheck)

--- src/smartcard/sw/ISO7816_9ErrorChecker.py
+++ src/smartcard/sw/ISO7816_9ErrorChecker.py
@@ -92,7 +92,7 @@ class ISO7816_9ErrorChecker(ErrorChecker):
 
         if sw1 in iso7816_9SW:
             exception, sw2dir = iso7816_9SW[sw1]
-            if type(sw2dir) == type({}):
+            if isinstance(sw2dir, dict):
                 try:
                     message = sw2dir[sw2]
                     raise exception(data, sw1, sw2, message)
