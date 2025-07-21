fae71b7b678fc5a30f34585c20fcf7b2402ef7f7
Ludovic Rousseau <ludovic.rousseau@free.fr>
2025-07-21 15:37:28 +0200

    sample_ConsoleConnectionTracer: ignore/fix pylint warning
    sample_ConsoleConnectionTracer.py:1:0: C0103: Module name "sample_ConsoleConnectionTracer" doesn't conform to snake_case naming style (invalid-name)
    sample_ConsoleConnectionTracer.py:60:0: C0413: Import "import sys" should be placed at the top of the module (wrong-import-position)
    sample_ConsoleConnectionTracer.py:60:0: C0411: standard import "sys" should be placed before third party imports "smartcard.CardConnectionObserver.ConsoleCardConnectionObserver", "smartcard.CardRequest.CardRequest", "smartcard.CardType.AnyCardType" (wrong-import-order)

--- src/smartcard/Examples/framework/sample_ConsoleConnectionTracer.py
+++ src/smartcard/Examples/framework/sample_ConsoleConnectionTracer.py
@@ -1,4 +1,7 @@
 #! /usr/bin/env python3
+
+# pylint: disable=invalid-name
+
 """
 Sample script that monitors card connection events.
 
@@ -23,6 +26,8 @@ You should have received a copy of the GNU Lesser General Public License
 along with pyscard; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 """
+import sys
+
 from smartcard.CardConnectionObserver import ConsoleCardConnectionObserver
 from smartcard.CardRequest import CardRequest
 from smartcard.CardType import AnyCardType
@@ -56,8 +61,6 @@ if sw1 == 0x9F:
 
 cardservice.connection.disconnect()
 cardservice.connection.release()
-
-import sys
 
 if "win32" == sys.platform:
     print("press Enter to continue")
