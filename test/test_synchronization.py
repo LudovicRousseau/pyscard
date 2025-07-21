f82fc75352534ce9a2bb791154b45b1d43f7c43d
Ludovic Rousseau <ludovic.rousseau@free.fr>
2025-07-21 14:45:32 +0200

    test_synchronization: ignore pylint warning
    test_synchronization.py:1:0: C0114: Missing module docstring (missing-module-docstring)
    test_synchronization.py:52:4: C0115: Missing class docstring (missing-class-docstring)
    test_synchronization.py:53:8: C0116: Missing function or method docstring (missing-function-docstring)
    test_synchronization.py:70:4: R0903: Too few public methods (1/2) (too-few-public-methods)

--- test/test_synchronization.py
+++ test/test_synchronization.py
@@ -1,3 +1,8 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+# pylint: disable=too-few-public-methods
+
 from __future__ import annotations
 
 import pytest
