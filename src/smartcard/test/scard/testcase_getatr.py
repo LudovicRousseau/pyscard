fad30034985bb5b698fc70d9bc17e0a25cdb9d4a
Ludovic Rousseau <ludovic.rousseau@free.fr>
2025-07-22 17:14:15 +0200

    testcase_getATR: fix pylint warning
    scard/testcase_getatr.py:74:28: W0612: Unused variable 'dwActiveProtocol' (unused-variable)
    scard/testcase_getatr.py:83:33: W0612: Unused variable 'state' (unused-variable)
    scard/testcase_getatr.py:83:40: W0612: Unused variable 'protocol' (unused-variable)

--- src/smartcard/test/scard/testcase_getatr.py
+++ src/smartcard/test/scard/testcase_getatr.py
@@ -71,7 +71,7 @@ class testcase_getATR(unittest.TestCase):
 
     def _getATR(self, r):
         if r < len(expectedATRs) and [] != expectedATRs[r]:
-            hresult, hcard, dwActiveProtocol = SCardConnect(
+            hresult, hcard, _dwActiveProtocol = SCardConnect(
                 self.hcontext,
                 self.readers[r],
                 SCARD_SHARE_SHARED,
@@ -80,7 +80,7 @@ class testcase_getATR(unittest.TestCase):
             self.assertEqual(hresult, 0)
 
             try:
-                hresult, reader, state, protocol, atr = SCardStatus(hcard)
+                hresult, reader, _state, _protocol, atr = SCardStatus(hcard)
                 self.assertEqual(hresult, 0)
                 self.assertEqual(reader, expectedReaders[r])
                 self.assertEqual(atr, expectedATRs[r])
