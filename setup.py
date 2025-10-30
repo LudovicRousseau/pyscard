9bafec0ec23ac40b2ae4f25a73499c6c0227a807
Jay Lee <jay0lee@gmail.com>
2025-10-30 17:38:19 +0000

    Python 3.14 support

--- setup.py
+++ setup.py
@@ -147,6 +147,7 @@ kw = {
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
         "Topic :: Security",
     ],
 }
